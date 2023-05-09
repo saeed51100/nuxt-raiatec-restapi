fb30387f2dbe8aef3dce43a4b6afca3927714f79
saeed <saeedblack77@gmail.com>
2023-05-09 22:31:16 +0330

    khoone 1402/02/19 17:39 56:04

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,21 @@
 <template>
   <main>
     <section class="hero bg-gray-100 py-10">
-      <div class="container flex">
-        <!--        img-->
+      <div class="container flex item-center">
+        <!--        Image -->
+        <div class="w-[200px] relative h-[200px] mr-5 rounded overflow-hidden shadow-md">
+          <img src="~assets/img/42565948.jpg"
+               class="w-full h-full absolute object-cover">
+        </div>
         <!--      Header text-->
         <div>
           <h1 class="text-4xl font-bold mb-2">Elon Musk</h1>
-          <p>I built SpaceX, acquired Tesla, Twitter.<br/>
+          <p class="mb-3">I built SpaceX, acquired Tesla, Twitter.<br/>
             In my free time i sh*t post on twitter.
           </p>
+          <a class="btn bg-sky-500 text-sky-50 py-2 px-4 rounded inline-block hover:bg-sky-600 duration-200"
+             href="https://github.com/saeed51100">My Github
+          </a>
         </div>
       </div>
     </section>
