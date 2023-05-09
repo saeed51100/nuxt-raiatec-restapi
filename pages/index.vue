c9ff6eba2b3c0f4e8ccd027cd1b2a54e36186cde
saeed <saeedblack77@gmail.com>
2023-05-09 21:55:20 +0330

    khoone 1402/02/19 17:39 48:14

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <script lang="ts" setup>
-
 </script>
 
 <template>
+  <main>
+    <section class="hero bg-gray-100 py-10">
+      <div class="container flex">
+        <!--        img-->
+        <!--      Header text-->
+        <div>
+          <h1 class="text-4xl font-bold mb-2">Elon Musk</h1>
+          <p>I built SpaceX, acquired Tesla, Twitter.<br/>
+            In my free time i sh*t post on twitter.
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
   <h1>Index File</h1>
 </template>
