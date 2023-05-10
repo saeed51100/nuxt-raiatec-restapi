8d66269c954fcc24c41cab0c3b074d20ec9e890c
saeed <saeedblack77@gmail.com>
2023-05-10 20:58:37 +0330

    khoone 1402/02/20 19:46 1:26:13

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,7 +1,15 @@
-<script lang="ts" setup>
-
-</script>
-
+<script lang="ts" setup></script>
 <template>
   <PageHeader title="Category Index"/>
+  <section class="py-10 container">
+    <div class="flex flex-wrap gap-5">
+      <NuxtLink
+          v-for="i in 33"
+          :key="i"
+          to="/categories/1234"
+          class="block py-2 px-4 bg-gray-300 rounded hover:bg-gray-400">
+        #CategoryName{{ i }}
+      </NuxtLink>
+    </div>
+  </section>
 </template>
