73b61438f31f1a6b7fba9081a87c70c06aa17f38
saeed <saeedblack77@gmail.com>
2023-05-12 14:10:18 +0330

    khoone 1402/02/22 14:10 1:49:44

--- composables/useWpApi.ts
+++ composables/useWpApi.ts
@@ -1,38 +1,48 @@
+/**
+ * WordPress Composables
+ * A collection of WordPress composable functions.
+ */
+
+import { Post } from "~~/types/post";
+
 export default () => {
     const config = useRuntimeConfig();
-    const wpUri = config.public.wpUri;
-    // get
-    const get = async <T>(endpoint: string) =>
-        useFetch<T>(`${wpUri}/wp-json/wp/v2/${endpoint}`);
+    const WP_URL: string = config.wpUri;
 
-    // Get All Posts
-    const getPosts = async <T>(
-        categories: number,
+    const get = async <T>(endpoint: string) => {
+        return useFetch<T>(`${WP_URL}/wp-json/wp/v2/${endpoint}`);
+    };
+
+    const getPosts = async (
+        category?: number,
         page: number = 1,
-        perPage: number = 9
+        perPgae: number = 20,
+        fields: string = "author,id,excerpt,title,link,slug,date"
     ) => {
-        let query = `posts?_embed&per_page=${perPage}&page=${page}`;
-        if (categories) {
-            query += `&categories=${categiries}`;
+        let query: string = `posts?page=${page}&per_page=${perPgae}&_embed=1`;
+        if (category) {
+            query += `&categories=${category}`;
         }
-        return get<T>(query);
-    }
+        return get<Post[]>(query);
+    };
 
-    // Get a Single Posts
-    const getPost = async (slug: string) => get(`posts?slug=${slug}&_embed`);
+    const getPost = async (slug: string) => {
+        return get<Post[]>(`posts?slug=${slug}&_embed=1`);
+    };
 
-    // Get All Categories
-    const getCategories = async (slug: string) => get(`categories`);
+    const getCatgories = async (fields: string = "name,slug,count") => {
+        return get<any>(`categories`);
+    };
 
-    // Get a Single Category
-    const getCategory = async (slug: string) => get(`categories?slug=${slug}`);
+    const getCatgory = async (slug: string) => {
+        return get<any>(`categories?slug=${slug}`);
+    };
 
     return {
         get,
         getPosts,
         getPost,
-        getCategories,
-        getCategory,
+        getCatgories,
+        getCatgory,
     };
-
 };
