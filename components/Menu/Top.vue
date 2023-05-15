dd276a5305f8f744b75df8045e88864b6c739aff
saeed <saeedblack77@gmail.com>
2023-05-15 19:59:23 +0330

    khoone 1402/02/25 19:26

--- components/Menu/Top.vue
+++ components/Menu/Top.vue
@@ -1,75 +1,37 @@
-<script lang="ts" setup>
-export default {
-  name: 'App',
-  data(){
-    return {
-      mySVG: require('./assets/raiatec.svg')
-    }
-  }
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <header class="p-1 bg-light sticky-top shadow">
-
-    <div class="d-flex flex-wrap align-items-center justify-content-around">
-
-      <a href="http://localhost/test.raiatec.com/"
-         class="mb-2 mb-lg-0 text-white text-decoration-none">
-        <img src="mySVG" class="<!--bi me-2-->" width="0" height="0" role="img" aria-label="Bootstrap">
-
-
-      </a>
-
-      <?php if (!is_page('about-us')): ?>
-
-      <!-- Button trigger modal -->
-      <button class="navbar navbar-toggler d-md-none collapsed"
-              type="button"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-              aria-controls="sidebarMenu"
-              aria-expanded="false"
-              aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg bg-info">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-
-
-      <?php endif ?>
-
-      <form class="d-none d-md-block" role="search">
-        <input type="search" class="form-control bg-light" placeholder="Search..."
-               aria-label="Search">
-      </form>
-
-      <ul class="nav justify-content-center">
-
-
-        <li>
-          <a href="http://localhost/test.raiatec.com/"
-          <?php echo(is_home() ? 'class="list-groop"' : ''); ?> >
-          Home
-          </a>
-        </li>
-
-        <li><NuxtLink to="/about">About Me</NuxtLink></li>
-
-
-        <!--                <li><a href="http://localhost/test.raiatec.com/about-us/" class="nav-link px-2 text-white">About Me</a></li>-->
-      </ul>
-
-
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown link
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
-
-  </header>
-
-
-
-
-
-
-
-
+  </nav>
   <nav class="bg-gray-900 text-white py-5">
     <div class="container flex justify-between">
       <NuxtLink to="/" class="logo text-xl font-bold">raiatec.com</NuxtLink>
@@ -81,4 +43,3 @@ export default {
     </div>
   </nav>
 </template>
-
