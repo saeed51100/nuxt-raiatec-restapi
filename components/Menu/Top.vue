cc4e82bbe595c2761d30327e0fa893edb3a60507
saeed javadzadeh <saeedblack77@gmail.com>
2023-05-17 00:27:31 +0330

    laptop 1402/02/26 0:27

--- components/Menu/Top.vue
+++ components/Menu/Top.vue
@@ -1,46 +1,53 @@
 <script lang="ts" setup></script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-info">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <header class="p-1 bg-light sticky-top shadow">
+
+    <div class="d-flex flex-wrap align-items-center justify-content-around">
+
+      <img src="~assets/img/raiatec.svg" alt="Raiatec Logo"/>
+
+
+      <!-- Button trigger modal -->
+      <button class="navbar navbar-toggler d-md-none collapsed"
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              aria-controls="sidebarMenu"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <nav class="bg-gray-900 text-white py-5">
-    <div class="container flex justify-between">
-      <NuxtLink to="/" class="logo text-xl font-bold">raiatec.com</NuxtLink>
-      <ul class="inline-flex gap-2">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/categories">Categories</NuxtLink></li>
-        <li><NuxtLink to="/about">About Me</NuxtLink></li>
-        <img src="~assets/img/raiatec.svg" alt="Raiatec Logo" />
+
+
+      <form class="d-none d-md-block" role="search">
+        <input type="search" class="form-control bg-light" placeholder="Search..."
+               aria-label="Search">
+      </form>
+
+      <ul class="nav justify-content-center">
+
+
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/categories">Categories</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">About Me</NuxtLink>
+        </li>
+
+
+        <!--                <li><a href="http://localhost/test.raiatec.com/about-us/" class="nav-link px-2 text-white">About Me</a></li>-->
       </ul>
+
+
     </div>
-  </nav>
+
+  </header>
 </template>
+
+<style>
+
+</style>
